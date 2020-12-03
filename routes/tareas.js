9f59bc7ba9e4fe25da0b6b3764213703ad4fb6c8
Aurelio Martín <Developer@MBP-L53C-73CH.local>
2020-12-03 22:34:16 +0000

    MERN full stack project

---
+++ routes/tareas.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const tareaController = require('../controllers/tareaController');
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
+
+// Crear una nueva tarea
+// api/tareas
+router.post('/',
+  auth,
+  [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('proyecto', 'El proyecto es obligatorio').not().isEmpty()
+  ],
+  tareaController.crearTarea
+);
+
+// Obtener las tareas por proyecto
+router.get('/', 
+  auth,
+  tareaController.obtenerTareas)
+
+// Actualizar tarea
+router.put('/:id', 
+  auth,
+  tareaController.actualizarTareas)
+
+// Eliminar tarea
+router.delete('/:id', 
+  auth,
+  tareaController.eliminarTareas)
+
+module.exports = router;
